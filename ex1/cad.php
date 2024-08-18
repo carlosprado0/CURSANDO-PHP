01e148e6db14275fecd67157c176ea1a7da8c042
Carlos Eduardo Prado Leite <ibroonkzz@gmail.com>
2024-08-18 11:02:36 -0300

    melhorando o formulario

---
+++ ex1/cad.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Resultado</title>
+</head>
+<body>
+    <header>
+
+        <h1>Resultado do processamento</h1>
+
+    </header>
+    <main>
+
+        <?php 
+            $n = $_GET["nome"]; // $_REQUEST funciona para os cookie, post e get.
+            $s = $_GET["sobrenome"];
+            echo "<p>É um prazer ti conhecer <strong>$n $s</strong>!";
+        ?>
+
+        <p><a href="javascript:history.go(-1)">Voltar à pagina anterior</a></p>
+
+    </main>
+</body>
+</html>
